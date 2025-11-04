b1baa80b812b403d9a40b05a012be4415b54cb02
Marco Schmidt <marco.schmidt@w3block.io>
2025-11-04 09:20:34 -0300

    Revert "adjusts"
    This reverts commit e46df85c5cc0939de61714816231c04091afa645.

--- node/connector.ts
+++ node/connector.ts
@@ -38,39 +38,12 @@ export default class TestSuiteApprover extends PaymentProvider {
   // Refer to https://help.vtex.com/en/tutorial/payment-provider-protocol#4-testing
   // in order to learn about the protocol and make the according changes.
 
-  private readonly APP_NAME = 'zuca-payment-app'
-
-  private patchPaymentAppData(
-    resp: AuthorizationResponse
-  ): AuthorizationResponse {
-    // Only pending/redirect responses have paymentAppData per typings,
-    // but we defensively patch when the field exists.
-    type WithPaymentAppData = {
-      paymentAppData?: { appName?: string; payload?: string } | null
-    }
-
-    const patchable = resp as AuthorizationResponse & WithPaymentAppData
-
-    if ('paymentAppData' in patchable) {
-      const current = patchable.paymentAppData
-
-      patchable.paymentAppData = {
-        ...(current ?? {}),
-        appName: this.APP_NAME,
-      }
-    }
-
-    return patchable as AuthorizationResponse
-  }
-
   private async saveAndRetry(
     req: AuthorizationRequest,
     resp: AuthorizationResponse
   ) {
-    const patched = this.patchPaymentAppData(resp)
-
-    await persistAuthorizationResponse(this.context.clients.vbase, patched)
-    this.callback(req, patched)
+    await persistAuthorizationResponse(this.context.clients.vbase, resp)
+    this.callback(req, resp)
   }
 
   public async authorize(
@@ -83,14 +56,12 @@ export default class TestSuiteApprover extends PaymentProvider {
       )
 
       if (persistedResponse !== undefined && persistedResponse !== null) {
-        return this.patchPaymentAppData(persistedResponse)
+        return persistedResponse
       }
 
-      const initial = executeAuthorization(authorization, response =>
+      return executeAuthorization(authorization, response =>
         this.saveAndRetry(authorization, response)
       )
-
-      return this.patchPaymentAppData(initial)
     }
 
     throw new Error('Not implemented')
